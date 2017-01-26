9e77ab62d7e6d0fec206c4300a3d1b8b5cae806e
Prajit Nadkarni <prajit.pn@flipkart.com>
2017-01-26 21:47:28 +0530

    - updated pom groupId and version

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@
 [ -z "$1" ] && echo $'Usage: \n1: Run From project root. \n2: ./scripts/run.sh <Path of directory which contains 3 files, positive.txt, negative.txt, reviews.txt>' && exit 1
 
 mvn clean install assembly:single
-java -jar target/ReviewAspectExtraction-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1
+java -jar target/ReviewAspectExtraction-1.0.0-SNAPSHOT-jar-with-dependencies.jar $1
